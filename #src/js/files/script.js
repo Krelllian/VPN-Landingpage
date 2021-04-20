8887726bbc0a3f12b5a4abb052b1fd8626a019cd
Krelllian <65556888+Krelllian@users.noreply.github.com>
2021-04-20 20:32:44 +0300

    slider and footer

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,14 +1,23 @@
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
-    //slidesPerView: 2,
+    slidesPerView: 2.4,
     loop: true,
+    //effect: 'fade',
+
+    //fadeEffect: {
+    //    crossFade: true,
+    //},
+    //autoHeight: false,
+
+    spaceBetween: 50,
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+
+        clickable: true,
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
--- VPN-Landingpage/js/app.js
+++ VPN-Landingpage/js/app.js
@@ -3,15 +3,24 @@ function email_test(input) {
 }
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
-    //slidesPerView: 2,
+    slidesPerView: 2.4,
     loop: true,
+    //effect: 'fade',
+
+    //fadeEffect: {
+    //    crossFade: true,
+    //},
+    //autoHeight: false,
+
+    spaceBetween: 50,
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+
+        clickable: true,
     },
 
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
